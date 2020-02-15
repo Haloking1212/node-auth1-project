872f16dc99b3fda5b7de2e74b1f48751b1543541
Hector Valdez <hvaldez180@gmail.com>
2020-02-14 21:52:31 -0500

    added cookies

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,10 +23,28 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', authorize, (req, res) => {
-  // let { username } = req.body;
-  res.status(200).json();
-})
+// router.post('/login', authorize, (req, res) => {
+//   res.status(200).json();
+// })
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedin = true;
+        res.status(200).json({ message: `Welcome ${user.username}! have a... biscuit.`, });
+      } else {
+        res.status(401).json({ message: 'Nice try. But, no. Try. Try again.' });
+      }
+    })
+    .catch(error => {   
+      res.status(500).json(error);
+    });
+});
 
 
 module.exports = router;
--- users/users-router.js
+++ users/users-router.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const Users = require('./users-model.js');
 const authrequired = require('../auth/auth-required-middleware.js');
 
-router.get('/', authrequired, (req, res) => {
+const restricted = require('../auth/restricted-middleware.js')
+
+router.get('/', authrequired, restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
